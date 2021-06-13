bf2bd4d0331b54dcda6aaf2c8c56e990fc53d952
Michal Dvorak <mikee2185@gmail.com>
2021-06-13 16:27:49 +0200

    reporting water level via prometheus

--- main/app_main.c
+++ main/app_main.c
@@ -1,14 +1,10 @@
 #include "app_status.h"
-#include <app_rainmaker.h>
 #include <app_wifi.h>
 #include <double_reset.h>
 #include <driver/adc_common.h>
-#include <driver/touch_sensor.h>
 #include <ds18b20.h>
 #include <esp_http_server.h>
 #include <esp_log.h>
-#include <esp_rmaker_core.h>
-#include <esp_rmaker_standard_params.h>
 #include <esp_wifi.h>
 #include <nvs_flash.h>
 #include <owb.h>
@@ -17,12 +13,27 @@
 #include <wifi_reconnect.h>
 
 #define APP_DEVICE_NAME CONFIG_APP_DEVICE_NAME
-#define APP_DEVICE_TYPE CONFIG_APP_DEVICE_TYPE
 #define APP_CONTROL_LOOP_INTERVAL CONFIG_APP_CONTROL_LOOP_INTERVAL
 #define HW_DS18B20_PIN CONFIG_HW_DS18B20_PIN
-#define HW_SOIL_SENSOR_TOUCH_PAD CONFIG_HW_SOIL_SENSOR_TOUCH_PAD
-#define SENSORS_RMT_CHANNEL_TX RMT_CHANNEL_0
-#define SENSORS_RMT_CHANNEL_RX RMT_CHANNEL_1
+#define HW_SENSOR_ENABLE_PIN CONFIG_HW_SENSOR_ENABLE_PIN
+#define HW_SOIL_SENSOR_ADC1_CHANNEL CONFIG_HW_SOIL_SENSOR_ADC1_CHANNEL
+#define HW_WATER_LEVEL_SENSOR_COUNT CONFIG_HW_WATER_LEVEL_SENSOR_COUNT
+
+static adc1_channel_t HW_WATER_LEVEL_ADC1_CHANNELS[HW_WATER_LEVEL_SENSOR_COUNT] = {
+    CONFIG_HW_WATER_LEVEL_ADC1_CHANNEL_1,
+#if CONFIG_HW_WATER_LEVEL_SENSOR_COUNT > 1
+    CONFIG_HW_WATER_LEVEL_ADC1_CHANNEL_2,
+#endif
+#if CONFIG_HW_WATER_LEVEL_SENSOR_COUNT > 2
+    CONFIG_HW_WATER_LEVEL_ADC1_CHANNEL_3,
+#endif
+#if CONFIG_HW_WATER_LEVEL_SENSOR_COUNT > 3
+    CONFIG_HW_WATER_LEVEL_ADC1_CHANNEL_4,
+#endif
+#if CONFIG_HW_WATER_LEVEL_SENSOR_COUNT > 4
+    CONFIG_HW_WATER_LEVEL_ADC1_CHANNEL_5,
+#endif
+};
 
 static const char TAG[] = "app_main";
 
@@ -31,12 +42,25 @@ static httpd_handle_t httpd = NULL;
 static owb_rmt_driver_info owb_driver = {};
 static DS18B20_Info temperature_sensor = {};
 static float temperature_value = 0;
-static uint16_t soil_value = 0;
-static int soil_adc_value = 0;
+static int soil_humidity_value = 0;
+static int water_level_values[HW_WATER_LEVEL_SENSOR_COUNT] = {};
+static float water_level = 0.0f;
+static int water_level_sensor_threshold = 1000; // TODO configurable both in Kconfig and remotely
 
 // Program
-static void app_devices_init(esp_rmaker_node_t *node);
 static esp_err_t metrics_http_handler(httpd_req_t *r);
+
+static void print_qrcode_handler(__unused void *arg, __unused esp_event_base_t event_base,
+                                 __unused int32_t event_id, __unused void *event_data)
+{
+    const char VER[] = "v1";
+    char payload[200] = {};
+    // {"ver":"%s","name":"%s","pop":"%s","transport":"%s"}
+    snprintf(payload, sizeof(payload), "%%7B%%22ver%%22%%3A%%22%s%%22%%2C%%22name%%22%%3A%%22%s%%22%%2C%%22pop%%22%%3A%%22%s%%22%%2C%%22transport%%22%%3A%%22%s%%22%%7D",
+             VER, app_wifi_prov_get_service_name(), app_wifi_get_prov_pop(), APP_WIFI_PROV_TRANSPORT);
+    // NOTE print this regardless of log level settings
+    printf("PROVISIONING: To view QR Code, copy paste the URL in a browser:\n%s?data=%s\n", "https://espressif.github.io/esp-jumpstart/qrcode.html", payload);
+}
 
 void setup()
 {
@@ -67,33 +91,30 @@ void setup()
     ESP_ERROR_CHECK(app_wifi_init(&wifi_cfg));
     ESP_ERROR_CHECK(esp_wifi_set_ps(WIFI_PS_MAX_MODEM));
     ESP_ERROR_CHECK(wifi_reconnect_start());
+    ESP_ERROR_CHECK(esp_event_handler_instance_register(WIFI_PROV_EVENT, WIFI_PROV_START, print_qrcode_handler, NULL, NULL));
 
-    // Soil humidity (Touch) sensor init
-    // TODO configurable
-    ESP_ERROR_CHECK(touch_pad_init());
-    ESP_ERROR_CHECK(touch_pad_set_voltage(TOUCH_HVOLT_2V4, TOUCH_LVOLT_0V5, TOUCH_HVOLT_ATTEN_0V));
-    ESP_ERROR_CHECK(touch_pad_config(HW_SOIL_SENSOR_TOUCH_PAD, 0));
-    ESP_ERROR_CHECK(touch_pad_filter_start(10));
+    // Sensor power config
+    ESP_ERROR_CHECK(gpio_reset_pin(HW_SENSOR_ENABLE_PIN));
+    ESP_ERROR_CHECK(gpio_set_direction(HW_SENSOR_ENABLE_PIN, GPIO_MODE_OUTPUT));
+    ESP_ERROR_CHECK(gpio_pulldown_en(HW_SENSOR_ENABLE_PIN));
 
-    // Soil humidity (ADC) sensor init
+    // Soil humidity and water level sensor init
     ESP_ERROR_CHECK(adc1_config_width(ADC_WIDTH_BIT_12));
-    ESP_ERROR_CHECK(adc1_config_channel_atten(ADC1_CHANNEL_0, ADC_ATTEN_DB_11)); // GPIO36
+    ESP_ERROR_CHECK(adc1_config_channel_atten(HW_SOIL_SENSOR_ADC1_CHANNEL, ADC_ATTEN_DB_11)); // TODO attenuation?
+
+    for (size_t i = 0; i < HW_WATER_LEVEL_SENSOR_COUNT; i++)
+    {
+        assert(HW_WATER_LEVEL_ADC1_CHANNELS[i] >= 0);
+        assert(HW_WATER_LEVEL_ADC1_CHANNELS[i] < ADC1_CHANNEL_MAX);
+        ESP_ERROR_CHECK(adc1_config_channel_atten(HW_WATER_LEVEL_ADC1_CHANNELS[i], ADC_ATTEN_DB_11));
+    }
 
     // Temperature sensor init
-    owb_rmt_initialize(&owb_driver, HW_DS18B20_PIN, SENSORS_RMT_CHANNEL_TX, SENSORS_RMT_CHANNEL_RX);
+    owb_rmt_initialize(&owb_driver, HW_DS18B20_PIN, RMT_CHANNEL_0, RMT_CHANNEL_1);
     owb_use_crc(&owb_driver.bus, true);
     ds18b20_init_solo(&temperature_sensor, &owb_driver.bus); // Only single sensor is expected
     ds18b20_use_crc(&temperature_sensor, true);
     ds18b20_set_resolution(&temperature_sensor, DS18B20_RESOLUTION_12_BIT);
-
-    // RainMaker
-    char node_name[APP_RMAKER_NODE_NAME_LEN] = {};
-    ESP_ERROR_CHECK(app_rmaker_node_name(node_name, sizeof(node_name)));
-
-    esp_rmaker_node_t *node = NULL;
-    ESP_ERROR_CHECK(app_rmaker_init(node_name, &node));
-
-    app_devices_init(node);
 
     // HTTP Server
     httpd_config_t httpd_config = HTTPD_DEFAULT_CONFIG();
@@ -102,8 +123,6 @@ void setup()
     ESP_ERROR_CHECK_WITHOUT_ABORT(httpd_register_uri_handler(httpd, &metrics_handler_uri));
 
     // Start
-    ESP_ERROR_CHECK(tcpip_adapter_set_hostname(TCPIP_ADAPTER_IF_STA, node_name)); // NOTE this isn't available before WiFi init
-    ESP_ERROR_CHECK(esp_rmaker_start());
     ESP_ERROR_CHECK(app_wifi_start(reconfigure));
 
     // Done
@@ -119,6 +138,9 @@ _Noreturn void app_main()
 
     for (;;)
     {
+        // Enable sensors
+        ESP_ERROR_CHECK_WITHOUT_ABORT(gpio_set_level(HW_SENSOR_ENABLE_PIN, 1));
+
         // Read temperature
         ds18b20_convert_all(&owb_driver.bus);
         ds18b20_wait_for_conversion(&temperature_sensor);
@@ -129,58 +151,39 @@ _Noreturn void app_main()
             ESP_LOGW(TAG, "failed to read temperature: %d", err);
         }
 
+        // Wait for sensors to stabilize (adds extra 100ms to the loop)
+        vTaskDelayUntil(&start, 100 / portTICK_PERIOD_MS);
+
         // Read soil humidity
-        ESP_ERROR_CHECK_WITHOUT_ABORT(touch_pad_read_filtered(HW_SOIL_SENSOR_TOUCH_PAD, &soil_value));
-        soil_adc_value = adc1_get_raw(ADC1_CHANNEL_0);
+        soil_humidity_value = adc1_get_raw(HW_SOIL_SENSOR_ADC1_CHANNEL);
+
+        // Read water levels
+        float water_level_readout = 0.0f;
+        for (size_t i = 0; i < HW_WATER_LEVEL_SENSOR_COUNT; i++)
+        {
+            int x = water_level_values[i] = adc1_get_raw(HW_WATER_LEVEL_ADC1_CHANNELS[i]);
+            if (x > water_level_sensor_threshold)
+            {
+                water_level_readout = (float)(i + 1) / (float)HW_WATER_LEVEL_SENSOR_COUNT;
+            }
+        }
+        water_level = water_level_readout;
+
+        // Disable sensors
+        // TODO
+        //  ESP_ERROR_CHECK_WITHOUT_ABORT(gpio_set_level(HW_SENSOR_ENABLE_PIN, 0));
 
         // Log output
-        ESP_LOGI(TAG, "soil1: %d, soil2: %d,\ttemperature: %.3f", soil_value, soil_adc_value, temperature_value);
+        ESP_LOGI(TAG, "water: %.1f, soil: %d,\ttemperature: %.3f", water_level_readout, soil_humidity_value, temperature_value);
 
         // Throttle
         vTaskDelayUntil(&start, APP_CONTROL_LOOP_INTERVAL / portTICK_PERIOD_MS);
     }
 }
 
-static esp_err_t device_write_cb(__unused const esp_rmaker_device_t *device, const esp_rmaker_param_t *param,
-                                 const esp_rmaker_param_val_t val, __unused void *private_data,
-                                 __unused esp_rmaker_write_ctx_t *ctx)
-{
-    //    char *param_name = esp_rmaker_param_get_name(param);
-    //    if (strcmp(param_name, "TODO") == 0)
-    //    {
-    //        // TODO handle
-    //        esp_rmaker_param_update_and_report(param, val);
-    //    }
-    return ESP_OK;
-}
-
-static void app_devices_init(esp_rmaker_node_t *node)
-{
-    // Prepare device
-    esp_rmaker_device_t *device = esp_rmaker_device_create(APP_DEVICE_NAME, APP_DEVICE_TYPE, NULL);
-    assert(device);
-
-    ESP_ERROR_CHECK(esp_rmaker_device_add_cb(device, device_write_cb, NULL));
-    ESP_ERROR_CHECK(esp_rmaker_device_add_param(device, esp_rmaker_name_param_create(ESP_RMAKER_DEF_NAME_PARAM, APP_DEVICE_NAME)));
-    ESP_ERROR_CHECK(esp_rmaker_node_add_device(node, device));
-
-    // Register buttons, sensors, etc
-}
-
 static esp_err_t metrics_http_handler(httpd_req_t *r)
 {
-    // Read device name from NVS, since rainmaker provides absolutely no means to get it directly
-    nvs_handle_t handle;
-    esp_err_t err = nvs_open(APP_DEVICE_NAME, NVS_READONLY, &handle);
-    if (err != ESP_OK)
-    {
-        ESP_LOGW(TAG, "failed to open nvs storage");
-        return err;
-    }
-    char name[100] = {};
-    size_t name_len = sizeof(name);
-    nvs_get_str(handle, ESP_RMAKER_DEF_NAME_PARAM, name, &name_len);
-    nvs_close(handle);
+    const char name[] = APP_DEVICE_NAME; // TODO dynamic from device config
 
     // Build metrics string
     char buf[1024] = {};
@@ -196,8 +199,18 @@ static esp_err_t metrics_http_handler(httpd_req_t *r)
 
     // Soil
     ptr = util_append(ptr, end, "# TYPE esp_humidity_raw gauge\n");
-    ptr = util_append(ptr, end, "esp_humidity_raw{hardware=\"%s\",sensor=\"Soil\"} %u\n", name, soil_value);
-    ptr = util_append(ptr, end, "esp_humidity_raw{hardware=\"%s\",sensor=\"ADC\"} %d\n", name, soil_adc_value);
+    ptr = util_append(ptr, end, "esp_humidity_raw{hardware=\"%s\",sensor=\"Soil\"} %d\n", name, soil_humidity_value);
+    // TODO provide normalized value
+
+    // Water level
+    ptr = util_append(ptr, end, "# TYPE esp_water_level_raw gauge\n");
+    for (size_t i = 0; i < HW_WATER_LEVEL_SENSOR_COUNT; i++)
+    {
+        ptr = util_append(ptr, end, "esp_water_level_raw{hardware=\"%s\",sensor=\"Pot %zu\"} %d\n", name, i, water_level_values[i]);
+    }
+
+    ptr = util_append(ptr, end, "# TYPE esp_water_level gauge\n");
+    ptr = util_append(ptr, end, "esp_water_level{hardware=\"%s\",sensor=\"Pot\"} %.2f\n", name, water_level);
 
     // Send result
     if (ptr != NULL)
